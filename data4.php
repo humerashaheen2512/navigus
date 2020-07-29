36d7a7def9327c9fb55576ffbca26d208bd01302
humerashaheen2512 <56517017+humerashaheen2512@users.noreply.github.com>
2020-07-29 13:34:19 +0530

    Update data4.php

--- data4.php
+++ data4.php
@@ -1,8 +1,8 @@
 <?php
-$username="root";
-$pass="";
+$username="o1qm1dx7spekawdo";
+$pass="gefy467mhssdhsag";
 
-$pdo=new PDO("mysql:host=localhost;dbname=register", $username, $pass);
+$pdo=new PDO("mysql:host=jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=y10j80f55bqwaug9", $username, $pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::FETCH_ASSOC, PDO::FETCH_OBJ);
 session_start();
